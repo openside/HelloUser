1073dc85d4ad3282a5673effc0506d188e3033c7
OpenSide Edu User <SM.MirEsmaeely@gmail.com>
2018-10-27 05:00:13 +0330

    Replaced "name" with "first name" and "last name"

--- src/ir/openuniverse/Main.java
+++ src/ir/openuniverse/Main.java
@@ -6,8 +6,12 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("What is your name?");
-		String name = scanner.nextLine();
-		System.out.println("Hello " + name + "!");
+		System.out.println("What is your first name?");
+		String firstName = scanner.nextLine();
+		
+		System.out.println("What is your last name?");
+		String lastName = scanner.nextLine();
+		
+		System.out.println("Hello " + firstName + " " + lastName + "!");
 	}
 }
